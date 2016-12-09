300517b7f71024a4267dd4715e4aba5b390f496d
DianaSusharnyk <DinkaSushik@gmail.com>
2016-12-09 21:09:16 +0200

    How to use template

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ $(function() {
     console.log()
     var ITEM_TEMPLATE = $(".bl-row-temp").html();
     var ITEM_TITLE = $(".temp").html();
-    var ITEM_TITLED;
-    var lIST;
-    var lIST_TITLE=;
-    var lIST_TITLED;
+    var ITEM_TITLED=$(".temped").html();
+    var lIST=$(".bl-list");
+    var lIST_TITLE=$(".bl-bought-temp");
+    var lIST_TITLED=$(".bl-boughted-temp");
     var number;
    
     });
