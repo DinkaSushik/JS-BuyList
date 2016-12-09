d309abdc921eb83b21beb616d7c2925be8a53c36
DianaSusharnyk <DinkaSushik@gmail.com>
2016-12-09 21:07:54 +0200

    AddItem

---
+++ main.js
@@ -0,0 +1,21 @@
+$(function() {
+    console.log()
+    var ITEM_TEMPLATE = $(".bl-row-temp").html();
+    var ITEM_TITLE = $(".temp").html();
+    var ITEM_TITLED;
+    var lIST;
+    var lIST_TITLE=;
+    var lIST_TITLED;
+    var number;
+   
+    });
+
+    
+    function addItem(title) {
+    }
+ 
+
+    addItem("Печиво");
+    addItem("Сир");
+    addItem("Помідори");
+    
